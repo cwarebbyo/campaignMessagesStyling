1bf7106c70989fc21bfd04dd8e2d218dc08a9c58
cwarebbyo <143715409+cwarebbyo@users.noreply.github.com>
2025-12-23 15:57:10 -0600

    Update campaignMessagesStyling.js

--- lwc/campaignMessagesStyling/campaignMessagesStyling.js
+++ lwc/campaignMessagesStyling/campaignMessagesStyling.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
-
-export default class CampaignMessagesStyling extends LightningElement {}
+import { loadStyle } from 'lightning/platformResourceLoader';
+import CAMPAIGN_OVERRIDES from '@salesforce/resourceUrl/campaignMessagesStyleOverride';
+export default class CampaignMessagesStyling extends LightningElement {
+  connectedCallback() {
+    loadStyle(this, CAMPAIGN_OVERRIDES);
+  }
+}
